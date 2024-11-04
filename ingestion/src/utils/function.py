316345f2b53700729eb8181fde6b922528e2f7ac
ELMEHDIEttaki <el.ettaki22@gmail.com>
2024-11-04 04:37:43 +0100

    producer module completed, now we'll move to check with kafka consumer

--- ingestion/src/utils/function.py
+++ ingestion/src/utils/function.py
@@ -8,6 +8,25 @@ import avro.schema
 import avro.io
 import json
 from kafka import KafkaProducer
+import logging
+import os
+
+
+# Create logs directory if it doesn't exist
+if not os.path.exists("logs"):
+    os.makedirs("logs")
+
+# Logger setup
+def setup_logger():
+    logging.basicConfig(
+        filename="logs/avro.log", 
+        level=logging.INFO, 
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    return logging.getLogger("AppLogger")
+
+logger = setup_logger()
+
 
 def load_producer(kafka_server):
     """Initialize Kafka producer."""
@@ -36,13 +55,44 @@ def adapt_message_for_avro(message, expected_fields):
     return message
 
 
-def avro_encode(data, avro_schema):
-    writer = avro.io.DatumWriter(avro_schema)
-    bytes_writer = io.BytesIO()
-    encoder = avro.io.BinaryEncoder(bytes_writer)
-    writer.write(data, encoder)
-    return bytes_writer.getvalue()
+# def avro_encode(data, avro_schema):
+#     writer = avro.io.DatumWriter(avro_schema)
+#     bytes_writer = io.BytesIO()
+#     encoder = avro.io.BinaryEncoder(bytes_writer)
+#     writer.write(data, encoder)
+#     return bytes_writer.getvalue()
 
+def avro_encode(data, avro_schema):
+    # Create a DatumWriter with the provided Avro schema
+    writer = avro.io.DatumWriter(avro_schema)
+    
+    # Create a BytesIO buffer to hold the serialized data
+    bytes_writer = io.BytesIO()
+    
+    # Create a BinaryEncoder that writes to the buffer
+    encoder = avro.io.BinaryEncoder(bytes_writer)
+    
+    # Write the data using the DatumWriter and BinaryEncoder
+    writer.write(data, encoder)
+    
+    # Get the raw bytes
+    avro_bytes = bytes_writer.getvalue()
+    
+    #logger.info(f"AVRO_BYTES: {avro_bytes}")
+    
+    # # Now we need to decode it back to a dictionary
+    # bytes_reader = io.BytesIO(avro_bytes)
+    # logger.info(f"BYTES_READER: {bytes_reader}")
+    # decoder = avro.io.BinaryDecoder(bytes_reader)
+    # logger.info(f"AVRO_DECODER_BYTES: {decoder}")
+    # reader = avro.io.DatumReader(avro_schema)
+    # logger.info(f"AVRO_BYTES_READER: {reader}")
+    
+    # # Read and return the data as a dictionary
+    # decoded_data = reader.read(decoder)
+    # logger.info(f"decoded_data: {decoded_data}")
+    
+    return avro_bytes
 
     # bytes_writer = io.BytesIO()
     # encoder = BinaryEncoder(bytes_writer)
