13e2c6621b0c8ea211404544f2a5746bb15dc23d
El Mehdi ETTAKI <156484307+ELMEHDIEttaki@users.noreply.github.com>
2024-11-15 21:40:25 +0000

    update

--- k8s-terraform/kafdrop/output.tf
+++ k8s-terraform/kafdrop/output.tf
@@ -1,3 +1,12 @@
+# output "service_url" {
+#   value = kubernetes_service.kafdrop.status.load_balancer[0].ingress[0].hostname
+# }
+
+# kafdrop/outputs.tf
 output "service_url" {
-  value = kubernetes_service.kafdrop.status.load_balancer[0].ingress[0].hostname
+  value = try(
+    kubernetes_service.kafdrop.status[0].load_balancer[0].ingress[0].hostname,
+    ""
+  )
+  description = "The URL of the Kafdrop service"
 }
